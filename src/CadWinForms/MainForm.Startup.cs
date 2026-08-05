5a52a354b0564003836af25240668aababaefe8f
zhangly1403 <zhangly1403@qq.com>
2026-08-05 17:09:07 +0800

    Apply WinForms layout before first paint

---
+++ src/CadWinForms/MainForm.Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace CadWinForms;
+
+public sealed partial class MainForm
+{
+    protected override void OnLoad(EventArgs e)
+    {
+        SuspendLayout();
+        try
+        {
+            _initialPanelLayoutApplied = ApplyInitialPanelLayout();
+        }
+        finally
+        {
+            ResumeLayout(performLayout: true);
+        }
+
+        base.OnLoad(e);
+    }
+}
