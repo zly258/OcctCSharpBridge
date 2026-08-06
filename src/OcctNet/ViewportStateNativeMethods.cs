872172d5df15e1675116a4b0ba382874cedf9c54
zhangly1403 <zhangly1403@qq.com>
2026-08-06 16:58:48 +0800

    Sync WPF host and API 2.3 wrapper from main

---
+++ src/OcctNet/ViewportStateNativeMethods.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OcctNet;
+
+internal static partial class NativeMethods
+{
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+    internal static extern int occt_get_viewport_state(IntPtr handle, out OcctViewportState result);
+
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+    internal static extern int occt_reset_view(IntPtr handle);
+
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+    internal static extern int occt_reset_view_orientation(IntPtr handle);
+
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+    internal static extern int occt_reset_view_mapping(IntPtr handle);
+
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+    internal static extern int occt_fit_selected(IntPtr handle, double margin);
+
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+    internal static extern int occt_get_scene_gravity_point(IntPtr handle, out OcctPoint3d result);
+}
