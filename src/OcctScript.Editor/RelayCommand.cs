fadeed9c0f5bac8d918eb390948526aff07cf24e
zhangly1403 <zhangly1403@qq.com>
2026-08-07 13:59:41 +0800

    Remove unused RelayCommand event field

--- src/OcctScript.Editor/RelayCommand.cs
+++ src/OcctScript.Editor/RelayCommand.cs
@@ -4,7 +4,12 @@ namespace OcctScript.Editor;
 
 internal sealed class RelayCommand(Action<object?> execute) : ICommand
 {
-    public event EventHandler? CanExecuteChanged;
+    public event EventHandler? CanExecuteChanged
+    {
+        add { }
+        remove { }
+    }
+
     public bool CanExecute(object? parameter) => true;
     public void Execute(object? parameter) => execute(parameter);
 }
