4798d2e4feb348bf1efab57f84be6cde98e27fc6
zhangly1403 <zhangly1403@qq.com>
2026-08-07 22:12:58 +0800

    Use SafeHandle for native session ownership

---
+++ src/OcctNet/OcctSafeHandles.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+namespace OcctNet;
+
+internal sealed class OcctEngineSafeHandle : SafeHandleZeroOrMinusOneIsInvalid
+{
+    internal OcctEngineSafeHandle(IntPtr nativeHandle)
+        : base(ownsHandle: true)
+    {
+        SetHandle(nativeHandle);
+    }
+
+    protected override bool ReleaseHandle()
+    {
+        try
+        {
+            NativeMethods.occt_destroy(handle);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+
+internal sealed class OcctModelingSafeHandle : SafeHandleZeroOrMinusOneIsInvalid
+{
+    internal OcctModelingSafeHandle(IntPtr nativeHandle)
+        : base(ownsHandle: true)
+    {
+        SetHandle(nativeHandle);
+    }
+
+    protected override bool ReleaseHandle()
+    {
+        try
+        {
+            ModelNativeMethods.occt_model_destroy(handle);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
