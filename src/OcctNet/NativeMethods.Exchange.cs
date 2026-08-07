c85921cbfcd686df0c247e78a3e1e7873fe85591
zhangly1403 <zhangly1403@qq.com>
2026-08-07 22:26:50 +0800

    Split exchange native declarations by responsibility

---
+++ src/OcctNet/NativeMethods.Exchange.cs
@@ -0,0 +1,16 @@
+﻿namespace OcctNet;
+
+internal static partial class NativeMethods
+{
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_import_file(IntPtr handle, [MarshalAs(UnmanagedType.LPUTF8Str)] string path);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_import_step(IntPtr handle, [MarshalAs(UnmanagedType.LPUTF8Str)] string path);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_import_iges(IntPtr handle, [MarshalAs(UnmanagedType.LPUTF8Str)] string path);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_import_brep(IntPtr handle, [MarshalAs(UnmanagedType.LPUTF8Str)] string path);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_import_stl(IntPtr handle, [MarshalAs(UnmanagedType.LPUTF8Str)] string path);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern int occt_export_step(IntPtr handle, long shapeId, [MarshalAs(UnmanagedType.LPUTF8Str)] string path);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern int occt_export_all_step(IntPtr handle, [MarshalAs(UnmanagedType.LPUTF8Str)] string path);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern int occt_export_iges(IntPtr handle, long shapeId, [MarshalAs(UnmanagedType.LPUTF8Str)] string path);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern int occt_export_all_iges(IntPtr handle, [MarshalAs(UnmanagedType.LPUTF8Str)] string path);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern int occt_export_brep(IntPtr handle, long shapeId, [MarshalAs(UnmanagedType.LPUTF8Str)] string path);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern int occt_export_stl(IntPtr handle, long shapeId, [MarshalAs(UnmanagedType.LPUTF8Str)] string path, double linearDeflection, double angularDeflection, int asciiMode);
+}
