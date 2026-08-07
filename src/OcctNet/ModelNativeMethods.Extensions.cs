42b161ff30b363dc6d47b914f4019e8595e5d5fd
zhangly1403 <zhangly1403@qq.com>
2026-08-07 23:33:25 +0800

    Add managed declarations for Bridge 2.6 modeling extensions

---
+++ src/OcctNet/ModelNativeMethods.Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OcctNet;
+
+internal static partial class ModelNativeMethods
+{
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    internal static extern int occt_model_shape_is_same(IntPtr handle, long firstId, long secondId);
+
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    internal static extern int occt_model_shape_is_partner(IntPtr handle, long firstId, long secondId);
+
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    internal static extern int occt_model_shape_oriented_bounds(
+        IntPtr handle,
+        long shapeId,
+        int optimal,
+        out OcctOrientedBounds result);
+
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    internal static extern long occt_model_make_face_with_holes(
+        IntPtr handle,
+        long outerWireId,
+        [In] long[] innerWireIds,
+        int innerWireCount);
+
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    internal static extern long occt_model_trim_edge(
+        IntPtr handle,
+        long edgeId,
+        double firstParameter,
+        double lastParameter);
+
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    internal static extern long occt_model_offset_wire(
+        IntPtr handle,
+        long wireId,
+        double offset,
+        double altitude,
+        int joinType,
+        int openResult);
+}
