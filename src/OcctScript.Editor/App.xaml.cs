2c91cc66964d3db539b93b60f0de0eca8a73f405
zhangly1403 <zhangly1403@qq.com>
2026-08-07 12:30:47 +0800

    Fix WPF Application namespace collision

--- src/OcctScript.Editor/App.xaml.cs
+++ src/OcctScript.Editor/App.xaml.cs
@@ -3,7 +3,7 @@ using System.Windows.Threading;
 
 namespace OcctScript.Editor;
 
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
     protected override void OnStartup(StartupEventArgs e)
     {
