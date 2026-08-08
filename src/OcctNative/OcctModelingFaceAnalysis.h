c384f06d6eb6e404c0b3c5a6d2e5b56a3b25006a
zhangly1403 <zhangly1403@qq.com>
2026-08-08 14:16:30 +0800

    Sync batch face analysis ABI

---
+++ src/OcctNative/OcctModelingFaceAnalysis.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "OcctModeling.h"
+
+extern "C"
+{
+    struct OcctModelFaceAnalysis
+    {
+        OcctObjectId faceId;
+        int surfaceType;
+        int orientation;
+        int edgeCount;
+        int wireCount;
+        double area;
+        double maximumTolerance;
+        OcctUvBounds uvBounds;
+        OcctBounds bounds;
+    };
+
+    OCCTBRIDGE_API int occt_model_shape_face_analysis(
+        OcctModelHandle handle,
+        OcctObjectId shapeId,
+        OcctModelFaceAnalysis* items,
+        int capacity,
+        int* count);
+}
