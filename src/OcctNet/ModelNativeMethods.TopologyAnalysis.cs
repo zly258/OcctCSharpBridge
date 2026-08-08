a53dc75b588c8c72c1473fa7f492c385f1b1c1a6
zhangly1403 <zhangly1403@qq.com>
2026-08-08 11:55:30 +0800

    Sync free-boundary analysis PInvoke contract

---
+++ src/OcctNet/ModelNativeMethods.TopologyAnalysis.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OcctNet;
+
+internal static partial class ModelNativeMethods
+{
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+    internal static extern long occt_model_shape_free_bounds(
+        IntPtr handle,
+        long shapeId,
+        double tolerance,
+        int boundaryKind,
+        int splitClosed,
+        int splitOpen);
+}
