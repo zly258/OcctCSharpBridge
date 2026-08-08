d5aa2c620ac840ba8ae0f4986085c79b4d04ba75
zhangly1403 <zhangly1403@qq.com>
2026-08-08 11:52:26 +0800

    Add strict free-boundary analysis ABI

---
+++ src/OcctNative/OcctModelingTopologyAnalysis.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "OcctModeling.h"
+
+extern "C"
+{
+    enum OcctModelFreeBoundaryKind
+    {
+        OcctModelFreeBoundary_Closed = 0,
+        OcctModelFreeBoundary_Open = 1
+    };
+
+    OCCTBRIDGE_API OcctObjectId occt_model_shape_free_bounds(
+        OcctModelHandle handle,
+        OcctObjectId shapeId,
+        double tolerance,
+        int boundaryKind,
+        int splitClosed,
+        int splitOpen);
+}
