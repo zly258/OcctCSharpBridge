82fd751984aa8d6b97efb545b1a0c99d893d23fc
zhangly1403 <zhangly1403@qq.com>
2026-08-09 17:22:39 +0800

    Move legacy bounds compatibility to extension API

---
+++ src/OcctNet/OcctGeometryExtensions.Compatibility.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace OcctNet;
+
+public static partial class OcctGeometryExtensions
+{
+    /// <summary>
+    /// Bridge 2.5 source-compatibility extension. New code should use
+    /// <see cref="OcctEngine.GetShapeBounds(OcctShape)"/>.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static OcctBounds GetBounds(
+        this OcctEngine engine,
+        OcctShape shape)
+    {
+        ArgumentNullException.ThrowIfNull(engine);
+        return engine.GetShapeBounds(shape);
+    }
+}
