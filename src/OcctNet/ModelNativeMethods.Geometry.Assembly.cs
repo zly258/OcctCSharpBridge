b53f4a629313d0292142e31b0b1d43b72deade6f
zhangly1403 <zhangly1403@qq.com>
2026-08-09 12:00:26 +0800

    Sync modeling session geometry split to demo
    Synchronize the validated OcctModelingSession and ModelNativeMethods geometry responsibility split from main to demo.

---
+++ src/OcctNet/ModelNativeMethods.Geometry.Assembly.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OcctNet;
+
+internal static partial class ModelNativeMethods
+{
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_compound(IntPtr handle, [In] long[] shapeIds, int count);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_wire(IntPtr handle, [In] long[] edgeIds, int count);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_sew(IntPtr handle, [In] long[] shapeIds, int count, double tolerance);
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_solid_from_shell(IntPtr handle, long shellId);
+}
