a7ec395603e7bed4e77276885954dda91eabeee1
zhangly1403 <zhangly1403@qq.com>
2026-08-10 09:03:27 +0800

    Add bulk history copy ABI

--- src/OcctNative/OcctModelingHistory.cpp
+++ src/OcctNative/OcctModelingHistory.cpp
@@ -20,6 +20,16 @@ extern "C"
         return result;
     }
 
+    int occt_model_history_generated_copy(OcctModelHandle handle, OcctOperationId operationId, OcctObjectId sourceShapeId, OcctObjectId* results, int capacity)
+    {
+        ModelSession* model = modelOf(handle);
+        if (model == nullptr) return -1;
+        int copied = 0;
+        if (execute(model, [&] { copied = historyCopy(model, operationId, sourceShapeId, true, results, capacity); }) == 0)
+            return -1;
+        return copied;
+    }
+
     int occt_model_history_modified_count(OcctModelHandle handle, OcctOperationId operationId, OcctObjectId sourceShapeId)
     {
         ModelSession* model = modelOf(handle);
@@ -34,6 +44,16 @@ extern "C"
         OcctObjectId result = 0;
         execute(model, [&] { result = historyShapeAt(model, operationId, sourceShapeId, index, false); });
         return result;
+    }
+
+    int occt_model_history_modified_copy(OcctModelHandle handle, OcctOperationId operationId, OcctObjectId sourceShapeId, OcctObjectId* results, int capacity)
+    {
+        ModelSession* model = modelOf(handle);
+        if (model == nullptr) return -1;
+        int copied = 0;
+        if (execute(model, [&] { copied = historyCopy(model, operationId, sourceShapeId, false, results, capacity); }) == 0)
+            return -1;
+        return copied;
     }
 
     int occt_model_history_is_removed(OcctModelHandle handle, OcctOperationId operationId, OcctObjectId sourceShapeId)
