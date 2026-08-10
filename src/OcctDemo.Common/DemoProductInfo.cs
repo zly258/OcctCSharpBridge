188ec55e9beffe72d9faf6725199a90127446735
zhangly1403 <zhangly1403@qq.com>
2026-08-10 18:03:25 +0800

    Read Bridge version dynamically in demo About

--- src/OcctDemo.Common/DemoProductInfo.cs
+++ src/OcctDemo.Common/DemoProductInfo.cs
@@ -1,3 +1,5 @@
+using OcctNet;
+
 namespace OcctDemo.Common;
 
 public static class DemoProductInfo
@@ -5,17 +7,17 @@ public static class DemoProductInfo
     public const string ProductName = "OcctCSharpBridge Demo";
     public const string Author = "zly258";
     public const string Email = "zhangly1403@gmail.com";
-    public const string BridgeVersion = "2.6.0";
-    public const int NativeAbiVersion = 4;
-    public const string OcctVersion = "7.9.0";
     public const string DotNetVersion = ".NET 10";
-    public const string DotNetSdkVersion = "10.0.302";
     public const string CSharpVersion = "C# 14";
     public const string NativeLanguage = "C++17";
     public const string Platform = "Windows x64";
     public const string AvaloniaVersion = "12.1.0";
     public const string Repository = "https://github.com/zly258/OcctCSharpBridge";
     public const string License = "PolyForm Noncommercial License 1.0.0";
+
+    public static string BridgeVersion => OcctBridgeInfo.ManagedVersion;
+    public static int NativeAbiVersion => OcctBridgeInfo.ExpectedAbiVersion;
+    public static string OcctVersion => OcctEngine.OcctVersion;
 
     public static string AboutText(DemoLanguage language) => language == DemoLanguage.ChineseSimplified
         ? $"OCCT CAD 演示应用\nBridge {BridgeVersion} · Native ABI {NativeAbiVersion}\nOCCT {OcctVersion} · {DotNetVersion} · {CSharpVersion} · {NativeLanguage}\nWinForms / WPF / Avalonia {AvaloniaVersion} · {Platform}\n\nRepository: {Repository}\nLicense: {License}\nAuthor: {Author}\nEmail: {Email}"
