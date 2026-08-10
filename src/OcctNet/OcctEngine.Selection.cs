97a093d4d171fccf71afb2b0d136b34c605dabd2
zhangly1403 <zhangly1403@qq.com>
2026-08-10 12:54:59 +0800

    Sync to restore the full Viewer selection interactive API

--- src/OcctNet/OcctEngine.Selection.cs
+++ src/OcctNet/OcctEngine.Selection.cs
@@ -2,6 +2,41 @@
 
 public sealed partial class OcctEngine
 {
+    public void MoveTo(int x, int y) =>
+        CheckInitialized(() => NativeMethods.occt_move_to(_handle, x, y));
+
+    public void Select(int x, int y, bool appendSelection = false) =>
+        CheckInitialized(() => NativeMethods.occt_select(_handle, x, y, appendSelection ? 1 : 0));
+
+    public void SelectRectangle(
+        int x1,
+        int y1,
+        int x2,
+        int y2,
+        bool appendSelection = false,
+        bool allowOverlap = false) =>
+        CheckInitialized(() => NativeMethods.occt_select_rectangle_ex(
+            _handle,
+            x1,
+            y1,
+            x2,
+            y2,
+            appendSelection ? 1 : 0,
+            allowOverlap ? 1 : 0));
+
+    public void SelectObject(IOcctObject value, bool appendSelection = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        EnsureObject(value);
+        CheckInitialized(() => NativeMethods.occt_select_object(_handle, value.Id, appendSelection ? 1 : 0));
+    }
+
+    public void SetSelectionMode(OcctSelectionMode mode)
+    {
+        if (!Enum.IsDefined(mode)) throw new ArgumentOutOfRangeException(nameof(mode));
+        CheckInitialized(() => NativeMethods.occt_set_selection_mode(_handle, (int)mode));
+    }
+
     public IReadOnlyList<IOcctObject> SelectedObjects
     {
         get
@@ -35,4 +70,13 @@ public sealed partial class OcctEngine
 
     public void ClearSelection() =>
         CheckInitialized(() => NativeMethods.occt_clear_selection(_handle));
+
+    public OcctShape CopySelectedSubshape() => CopySelectedSubshape(0);
+
+    public OcctShape CopySelectedSubshape(int index)
+    {
+        EnsureInitialized();
+        OcctGuard.PositiveIndex(index, nameof(index));
+        return CheckShape(NativeMethods.occt_copy_selected_subshape_at(_handle, index));
+    }
 }
