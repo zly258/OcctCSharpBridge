0f5a64506054285dce3a67b4f0e5d3705b3892c8
zhangly1403 <zhangly1403@qq.com>
2026-08-14 21:36:37 +0800

    Expose current modeling exchange ABI

--- src/OcctNative/include/occt_bridge/modeling.h
+++ src/OcctNative/include/occt_bridge/modeling.h
@@ -4,6 +4,7 @@
 #include "modeling/OcctModelingExtensions.h"
 #include "modeling/OcctModelingAnnotations.h"
 #include "modeling/OcctAlgorithmResource.h"
+#include "exchange/OcctModelingExchange.h"
 #include "topology/OcctModelingTopologyReference.h"
 #include "topology/OcctShapeResource.h"
 #include "mesh/OcctMeshResource.h"
