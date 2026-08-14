d3321a7a43c1c70a46f61281197d039e3d1cd628
zhangly1403 <zhangly1403@qq.com>
2026-08-14 20:25:02 +0800

    Test owned algorithm resource lifetime

---
+++ tests/OcctNet.ManagedTests/AlgorithmResourceTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OcctNet;
+
+namespace OcctNet.ManagedTests;
+
+[TestClass]
+public sealed class AlgorithmResourceTests
+{
+    [TestMethod]
+    public void AlgorithmDiagnosticsRemainAvailableAfterSessionDisposal()
+    {
+        OcctAlgorithmResource resource;
+        long operationId;
+
+        using (var session = new OcctModelingSession())
+        {
+            var left = session.MakeBox(20, 20, 20);
+            var right = session.MakeBox(new OcctPoint3d(10, 0, 0), 20, 20, 20);
+            var result = session.Fuse(left, right);
+            Assert.IsTrue(result.Succeeded);
+            Assert.IsFalse(result.HasErrors);
+
+            operationId = result.OperationId;
+            resource = session.AcquireAlgorithm(result);
+            Assert.AreEqual(operationId, resource.OperationId);
+            Assert.AreEqual(result.HasWarnings, resource.HasWarnings);
+            Assert.AreEqual(result.HasErrors, resource.HasErrors);
+            Assert.AreEqual(result.Report, resource.Report);
+        }
+
+        using (resource)
+        {
+            Assert.AreEqual(operationId, resource.OperationId);
+            Assert.IsFalse(resource.HasErrors);
+            _ = resource.Report;
+        }
+    }
+}
