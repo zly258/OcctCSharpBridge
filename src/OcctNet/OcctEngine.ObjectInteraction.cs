10774c5bc567cffb7f0ecc33f13dd05743403380
zhangly1403 <zhangly1403@qq.com>
2026-08-15 00:15:56 +0800

    Move object interaction and presentation actions to ABI5

--- src/OcctNet/OcctEngine.ObjectInteraction.cs
+++ src/OcctNet/OcctEngine.ObjectInteraction.cs
@@ -1,35 +1,70 @@
-﻿namespace OcctNet;
+using System.Runtime.InteropServices;
+
+namespace OcctNet;
 
 public sealed partial class OcctEngine
 {
-    public void SetSelectable(IOcctObject value, bool selectable)
+    public void SetObjectSelectable(IOcctObject value, bool selectable)
     {
         EnsureObject(value);
-        CheckInitialized(() => NativeMethods.occt_set_object_selectable(_handle, value.Id, selectable ? 1 : 0));
+        var options = ObjectUpdateOptions(NativeViewerObjectUpdateMask.Selectable);
+        options.Selectable = selectable ? 1 : 0;
+        UpdateObject(value.Id, options);
     }
 
-    public bool IsSelectable(IOcctObject value)
+    public void SetObjectsSelectable(IEnumerable<IOcctObject> values, bool selectable)
     {
-        EnsureObject(value);
-        return NativeMethods.occt_get_object_selectable(_handle, value.Id) != 0;
-    }
-
-    public void SetSelectable(IEnumerable<IOcctObject> values, bool selectable)
-    {
+        ArgumentNullException.ThrowIfNull(values);
+        EnsureInitialized();
         var ids = GetObjectIds(values, nameof(values));
-        if (ids.Length == 0) return;
-        CheckInitialized(() => NativeMethods.occt_set_objects_selectable(_handle, ids, ids.Length, selectable ? 1 : 0));
+        var options = ObjectUpdateOptions(NativeViewerObjectUpdateMask.Selectable);
+        options.Selectable = selectable ? 1 : 0;
+        foreach (var id in ids) UpdateObject(id, options);
     }
 
-    public void Highlight(IOcctObject value)
+    public bool IsObjectSelectable(IOcctObject value) => GetObjectState(value).Selectable != 0;
+
+    public bool IsObjectVisible(IOcctObject value) => GetObjectState(value).Visible != 0;
+
+    public bool IsObjectSelected(IOcctObject value) => GetObjectState(value).Selected != 0;
+
+    public bool IsObjectHighlighted(IOcctObject value) => GetObjectState(value).Highlighted != 0;
+
+    public void ShowAll()
     {
-        EnsureObject(value);
-        CheckInitialized(() => NativeMethods.occt_highlight_object(_handle, value.Id));
+        EnsureInitialized();
+        CheckObjectStatus(ObjectNativeMethods.occt_engine_objects_visibility_all_set(_handle, 1));
     }
 
-    public void Unhighlight(IOcctObject value)
+    public void HideAll()
+    {
+        EnsureInitialized();
+        CheckObjectStatus(ObjectNativeMethods.occt_engine_objects_visibility_all_set(_handle, 0));
+    }
+
+    public void Redisplay(IOcctObject value) =>
+        ApplyPresentationAction(value, NativeViewerObjectPresentationAction.Redisplay);
+
+    public void Highlight(IOcctObject value) =>
+        ApplyPresentationAction(value, NativeViewerObjectPresentationAction.Highlight);
+
+    public void Unhighlight(IOcctObject value) =>
+        ApplyPresentationAction(value, NativeViewerObjectPresentationAction.Unhighlight);
+
+    private NativeViewerObjectState GetObjectState(IOcctObject value)
     {
         EnsureObject(value);
-        CheckInitialized(() => NativeMethods.occt_unhighlight_object(_handle, value.Id));
+        EnsureInitialized();
+        CheckObjectStatus(ObjectNativeMethods.occt_engine_object_state_get(_handle, value.Id, out var state));
+        if (state.ApiVersion != 1 || state.StructSize < (uint)Marshal.SizeOf<NativeViewerObjectState>())
+            throw new OcctException("Native object state ABI is incompatible with this SDK.");
+        return state;
+    }
+
+    private void ApplyPresentationAction(IOcctObject value, NativeViewerObjectPresentationAction action)
+    {
+        EnsureObject(value);
+        EnsureInitialized();
+        CheckObjectStatus(ObjectNativeMethods.occt_engine_object_presentation_action(_handle, value.Id, action));
     }
 }
