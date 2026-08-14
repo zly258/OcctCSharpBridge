65a22dd5f26e9afcbeb59a01d119d44ce2685365
zhangly1403 <zhangly1403@qq.com>
2026-08-14 21:27:00 +0800

    Expose current viewer annotation ABI

--- src/OcctNative/include/occt_bridge/viewer.h
+++ src/OcctNative/include/occt_bridge/viewer.h
@@ -1,6 +1,7 @@
-#pragma once
+﻿#pragma once
 
 #include "OcctNative.h"
 #include "platform/OcctNativeSurface.h"
 #include "OcctRenderSurface.h"
 #include "OcctViewerInteraction.h"
+#include "presentation/OcctAnnotations.h"
