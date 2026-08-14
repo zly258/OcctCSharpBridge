f55aba2e470afad80305e028e8ff74c546e40575
zhangly1403 <zhangly1403@qq.com>
2026-08-15 00:13:28 +0800

    Move object appearance high-level API to ABI5

--- src/OcctNet/OcctEngine.ObjectAppearance.cs
+++ src/OcctNet/OcctEngine.ObjectAppearance.cs
@@ -1,56 +1,90 @@
-﻿using System.Drawing;
+using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace OcctNet;
 
 public sealed partial class OcctEngine
 {
-    public void SetColor(IOcctObject value, Color color)
+    public void SetObjectAppearance(IOcctObject value, OcctObjectAppearance appearance)
     {
+        ArgumentNullException.ThrowIfNull(appearance);
         EnsureObject(value);
-        CheckInitialized(() => NativeMethods.occt_set_object_color(
-            _handle,
-            value.Id,
-            color.R / 255.0,
-            color.G / 255.0,
-            color.B / 255.0));
+
+        var mask = NativeViewerObjectUpdateMask.Color |
+                   NativeViewerObjectUpdateMask.Transparency |
+                   NativeViewerObjectUpdateMask.Visibility |
+                   NativeViewerObjectUpdateMask.LineWidth |
+                   NativeViewerObjectUpdateMask.Material;
+        var options = ObjectUpdateOptions(mask);
+        options.Color = ToNativeObjectColor(appearance.Color);
+        options.Transparency = appearance.Transparency;
+        options.Visible = appearance.Visible ? 1 : 0;
+        options.LineWidth = appearance.LineWidth;
+        options.Material = (int)appearance.Material;
+        UpdateObject(value.Id, options);
+        SetDisplayModeOverride(value, appearance.DisplayMode);
     }
 
-    public void SetTransparency(IOcctObject value, double transparency)
+    public void SetObjectColor(IOcctObject value, Color color)
     {
         EnsureObject(value);
-        OcctGuard.UnitInterval(transparency, nameof(transparency));
-        CheckInitialized(() => NativeMethods.occt_set_object_transparency(_handle, value.Id, transparency));
+        var options = ObjectUpdateOptions(NativeViewerObjectUpdateMask.Color);
+        options.Color = ToNativeObjectColor(color);
+        UpdateObject(value.Id, options);
     }
 
-    public void SetVisible(IOcctObject value, bool visible)
+    public void SetObjectTransparency(IOcctObject value, double transparency)
     {
         EnsureObject(value);
-        CheckInitialized(() => NativeMethods.occt_set_object_visible(_handle, value.Id, visible ? 1 : 0));
+        if (!double.IsFinite(transparency) || transparency < 0.0 || transparency > 1.0)
+            throw new ArgumentOutOfRangeException(nameof(transparency));
+        var options = ObjectUpdateOptions(NativeViewerObjectUpdateMask.Transparency);
+        options.Transparency = transparency;
+        UpdateObject(value.Id, options);
     }
 
-    public void SetDisplayMode(IOcctObject value, OcctDisplayMode displayMode) =>
+    public void SetObjectVisible(IOcctObject value, bool visible)
+    {
+        EnsureObject(value);
+        var options = ObjectUpdateOptions(NativeViewerObjectUpdateMask.Visibility);
+        options.Visible = visible ? 1 : 0;
+        UpdateObject(value.Id, options);
+    }
+
+    public void SetObjectDisplayMode(IOcctObject value, OcctDisplayMode displayMode) =>
         SetDisplayModeOverride(value, displayMode);
 
-    public void SetLineWidth(IOcctObject value, double width)
+    public void SetObjectLineWidth(IOcctObject value, double width)
     {
         EnsureObject(value);
         OcctGuard.Positive(width, nameof(width));
-        CheckInitialized(() => NativeMethods.occt_set_object_line_width(_handle, value.Id, width));
+        var options = ObjectUpdateOptions(NativeViewerObjectUpdateMask.LineWidth);
+        options.LineWidth = width;
+        UpdateObject(value.Id, options);
     }
 
-    public void SetMaterial(IOcctObject value, OcctMaterial material)
+    public void SetObjectMaterial(IOcctObject value, OcctMaterial material)
     {
         EnsureObject(value);
         if (!Enum.IsDefined(material)) throw new ArgumentOutOfRangeException(nameof(material));
-        CheckInitialized(() => NativeMethods.occt_set_object_material(_handle, value.Id, (int)material));
+        var options = ObjectUpdateOptions(NativeViewerObjectUpdateMask.Material);
+        options.Material = (int)material;
+        UpdateObject(value.Id, options);
     }
 
-    public void ShowAll() => CheckInitialized(() => NativeMethods.occt_show_all(_handle));
-    public void HideAll() => CheckInitialized(() => NativeMethods.occt_hide_all(_handle));
-
-    public void Redisplay(IOcctObject value)
+    private static NativeViewerObjectUpdateOptions ObjectUpdateOptions(
+        NativeViewerObjectUpdateMask mask) => new()
     {
-        EnsureObject(value);
-        CheckInitialized(() => NativeMethods.occt_redisplay_object(_handle, value.Id));
-    }
+        StructSize = (uint)Marshal.SizeOf<NativeViewerObjectUpdateOptions>(),
+        ApiVersion = 1,
+        UpdateMask = mask,
+        LineWidth = 1.0
+    };
+
+    private static NativeViewColorRgb ToNativeObjectColor(Color value) => new()
+    {
+        R = value.R / 255.0,
+        G = value.G / 255.0,
+        B = value.B / 255.0
+    };
 }
