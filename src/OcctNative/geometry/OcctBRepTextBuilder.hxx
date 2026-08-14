f23e989149d302ffa6664314455f2fa4426d27ad
zhangly1403 <zhangly1403@qq.com>
2026-08-14 21:14:51 +0800

    Add headless BRep text builder

---
+++ src/OcctNative/geometry/OcctBRepTextBuilder.hxx
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "OcctNative.h"
+
+#include <Graphic3d_HorizontalTextAlignment.hxx>
+#include <Graphic3d_VerticalTextAlignment.hxx>
+#include <TopoDS_Shape.hxx>
+
+namespace OcctModelingInternal
+{
+    TopoDS_Shape buildBRepText(
+        const char* utf8Text,
+        const char* fontName,
+        OcctPoint3d position,
+        OcctVector3d normal,
+        OcctVector3d xDirection,
+        double height,
+        double extrusionDepth,
+        bool bold,
+        bool italic,
+        Graphic3d_HorizontalTextAlignment horizontalAlignment,
+        Graphic3d_VerticalTextAlignment verticalAlignment);
+}
