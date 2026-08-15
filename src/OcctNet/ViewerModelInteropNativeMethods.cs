95473d6bcf4154976fd90e7d9ea814efcc3e2021
zhangly1403 <zhangly1403@qq.com>
2026-08-15 08:41:08 +0800

    Add ABI5 viewer-model interop binding

---
+++ src/OcctNet/ViewerModelInteropNativeMethods.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace OcctNet;
+
+internal static partial class ViewerModelInteropNativeMethods
+{
+    private const string LibraryName = "OcctNative";
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_engine_object_shape_update_from_model(
+        OcctEngineSafeHandle engineHandle,
+        OcctModelingSafeHandle modelHandle,
+        long viewerObjectId,
+        long modelShapeId,
+        uint options);
+}
