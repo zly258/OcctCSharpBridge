96e81be302f692fcdb2422d61481c5e67252ce2e
zhangly1403 <zhangly1403@qq.com>
2026-08-15 11:24:26 +0800

    refactor(modeling): bind ABI5 transform API

--- src/OcctNet/ModelNativeMethods.Geometry.Transform.cs
+++ src/OcctNet/ModelNativeMethods.Geometry.Transform.cs
@@ -1,11 +1,43 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace OcctNet;
 
 internal static partial class ModelNativeMethods
 {
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_translate(OcctModelingSafeHandle handle, long shapeId, OcctVector3d vector);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_rotate(OcctModelingSafeHandle handle, long shapeId, OcctPoint3d axisPoint, OcctVector3d axisDirection, double angleDegrees);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_scale(OcctModelingSafeHandle handle, long shapeId, OcctPoint3d center, double factor);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_mirror_plane(OcctModelingSafeHandle handle, long shapeId, OcctPoint3d planePoint, OcctVector3d planeNormal);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_transform_translate(
+        OcctModelingSafeHandle handle,
+        long shapeId,
+        OcctVector3d vector,
+        out long result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_transform_rotate(
+        OcctModelingSafeHandle handle,
+        long shapeId,
+        OcctPoint3d axisPoint,
+        OcctVector3d axisDirection,
+        double angleDegrees,
+        out long result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_transform_scale(
+        OcctModelingSafeHandle handle,
+        long shapeId,
+        OcctPoint3d center,
+        double factor,
+        out long result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_transform_mirror_plane(
+        OcctModelingSafeHandle handle,
+        long shapeId,
+        OcctPoint3d planePoint,
+        OcctVector3d planeNormal,
+        out long result);
 }
