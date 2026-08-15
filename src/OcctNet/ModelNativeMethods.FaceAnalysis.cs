4cfc299fa75e345f1b7553f2fda81721bb6c44db
zhangly1403 <zhangly1403@qq.com>
2026-08-15 14:16:13 +0800

    Migrate face analysis to ABI5 snapshot contract

--- src/OcctNet/ModelNativeMethods.FaceAnalysis.cs
+++ src/OcctNet/ModelNativeMethods.FaceAnalysis.cs
@@ -8,10 +8,10 @@ internal static partial class ModelNativeMethods
 {
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_shape_face_analysis(
+    internal static partial OcctStatus occt_model_shape_face_analysis_snapshot_get(
         OcctModelingSafeHandle handle,
         long shapeId,
         [Out, MarshalUsing(CountElementName = nameof(capacity))] NativeModelFaceAnalysis[]? items,
         int capacity,
-        out int count);
+        out int required);
 }
