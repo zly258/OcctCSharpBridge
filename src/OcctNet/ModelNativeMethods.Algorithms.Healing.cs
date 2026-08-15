b802260e398c338a6d2bdfd8c1735e2c39442899
zhangly1403 <zhangly1403@qq.com>
2026-08-15 11:42:24 +0800

    refactor: migrate healing interop to LibraryImport ABI5

--- src/OcctNet/ModelNativeMethods.Algorithms.Healing.cs
+++ src/OcctNet/ModelNativeMethods.Algorithms.Healing.cs
@@ -1,9 +1,27 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace OcctNet;
 
 internal static partial class ModelNativeMethods
 {
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern NativeModelAlgorithmResult occt_model_unify_same_domain(OcctModelingSafeHandle handle, long shapeId, int unifyEdges, int unifyFaces, int concatBsplines);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern NativeModelAlgorithmResult occt_model_fix_shape(OcctModelingSafeHandle handle, long shapeId, double precision, double minTolerance, double maxTolerance);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_healing_unify_same_domain_execute(
+        OcctModelingSafeHandle handle,
+        long shapeId,
+        int unifyEdges,
+        int unifyFaces,
+        int concatBsplines,
+        out NativeModelAlgorithmResult result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_healing_fix_shape_execute(
+        OcctModelingSafeHandle handle,
+        long shapeId,
+        double precision,
+        double minTolerance,
+        double maxTolerance,
+        out NativeModelAlgorithmResult result);
 }
