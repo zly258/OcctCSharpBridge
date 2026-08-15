468eb56fcee1c09954e0a32f08635e053d3f8341
zhangly1403 <zhangly1403@qq.com>
2026-08-15 14:05:33 +0800

    Migrate geometry queries to ABI5 status contract

--- src/OcctNet/ModelNativeMethods.GeometryQueries.cs
+++ src/OcctNet/ModelNativeMethods.GeometryQueries.cs
@@ -7,29 +7,56 @@ internal static partial class ModelNativeMethods
 {
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_vertex_point(OcctModelingSafeHandle handle, long vertexId, out OcctPoint3d result);
+    internal static partial OcctStatus occt_model_vertex_point(
+        OcctModelingSafeHandle handle,
+        long vertexId,
+        out OcctPoint3d result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_endpoints(OcctModelingSafeHandle handle, long edgeId, out OcctPoint3d start, out OcctPoint3d end);
+    internal static partial OcctStatus occt_model_edge_endpoints(
+        OcctModelingSafeHandle handle,
+        long edgeId,
+        out OcctPoint3d start,
+        out OcctPoint3d end);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_point_at(OcctModelingSafeHandle handle, long edgeId, double normalizedParameter, out OcctPoint3d point, out OcctVector3d tangent);
+    internal static partial OcctStatus occt_model_edge_point_at(
+        OcctModelingSafeHandle handle,
+        long edgeId,
+        double normalizedParameter,
+        out OcctPoint3d point,
+        out OcctVector3d tangent);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_curve_type(OcctModelingSafeHandle handle, long edgeId);
+    internal static partial OcctStatus occt_model_edge_curve_type(
+        OcctModelingSafeHandle handle,
+        long edgeId,
+        out OcctCurveType result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_surface_type(OcctModelingSafeHandle handle, long faceId);
+    internal static partial OcctStatus occt_model_face_surface_type(
+        OcctModelingSafeHandle handle,
+        long faceId,
+        out OcctSurfaceType result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_uv_bounds(OcctModelingSafeHandle handle, long faceId, out OcctUvBounds result);
+    internal static partial OcctStatus occt_model_face_uv_bounds(
+        OcctModelingSafeHandle handle,
+        long faceId,
+        out OcctUvBounds result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_point_normal(OcctModelingSafeHandle handle, long faceId, double u, double v, out OcctPoint3d point, out OcctVector3d normal);
+    internal static partial OcctStatus occt_model_face_point_normal(
+        OcctModelingSafeHandle handle,
+        long faceId,
+        double u,
+        double v,
+        out OcctPoint3d point,
+        out OcctVector3d normal);
 }
