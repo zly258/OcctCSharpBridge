e61a9184e3c828c16a556392fa3c281730c5b8f0
zhangly1403 <zhangly1403@qq.com>
2026-08-15 11:59:26 +0800

    Migrate analytic geometry interop to LibraryImport

--- src/OcctNet/ModelNativeMethods.AnalyticGeometry.cs
+++ src/OcctNet/ModelNativeMethods.AnalyticGeometry.cs
@@ -1,30 +1,39 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace OcctNet;
 
 internal static partial class ModelNativeMethods
 {
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_edge_line_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctLineGeometry result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_edge_line_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctLineGeometry result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_edge_circle_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctCircleGeometry result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_edge_circle_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctCircleGeometry result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_edge_ellipse_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctEllipseGeometry result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_edge_ellipse_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctEllipseGeometry result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_plane_geometry(OcctModelingSafeHandle handle, long faceId, out OcctPlaneGeometry result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_plane_geometry(OcctModelingSafeHandle handle, long faceId, out OcctPlaneGeometry result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_cylinder_geometry(OcctModelingSafeHandle handle, long faceId, out OcctCylinderGeometry result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_cylinder_geometry(OcctModelingSafeHandle handle, long faceId, out OcctCylinderGeometry result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_cone_geometry(OcctModelingSafeHandle handle, long faceId, out OcctConeGeometry result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_cone_geometry(OcctModelingSafeHandle handle, long faceId, out OcctConeGeometry result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_sphere_geometry(OcctModelingSafeHandle handle, long faceId, out OcctSphereGeometry result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_sphere_geometry(OcctModelingSafeHandle handle, long faceId, out OcctSphereGeometry result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_torus_geometry(OcctModelingSafeHandle handle, long faceId, out OcctTorusGeometry result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_torus_geometry(OcctModelingSafeHandle handle, long faceId, out OcctTorusGeometry result);
 }
