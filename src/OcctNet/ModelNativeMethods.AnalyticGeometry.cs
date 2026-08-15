925fa1144f50215b7f9013e47e6082bee30df9f4
zhangly1403 <zhangly1403@qq.com>
2026-08-15 14:08:52 +0800

    Migrate analytic geometry queries to ABI5 status contract

--- src/OcctNet/ModelNativeMethods.AnalyticGeometry.cs
+++ src/OcctNet/ModelNativeMethods.AnalyticGeometry.cs
@@ -7,33 +7,33 @@ internal static partial class ModelNativeMethods
 {
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_line_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctLineGeometry result);
+    internal static partial OcctStatus occt_model_edge_line_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctLineGeometry result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_circle_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctCircleGeometry result);
+    internal static partial OcctStatus occt_model_edge_circle_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctCircleGeometry result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_ellipse_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctEllipseGeometry result);
+    internal static partial OcctStatus occt_model_edge_ellipse_geometry(OcctModelingSafeHandle handle, long edgeId, out OcctEllipseGeometry result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_plane_geometry(OcctModelingSafeHandle handle, long faceId, out OcctPlaneGeometry result);
+    internal static partial OcctStatus occt_model_face_plane_geometry(OcctModelingSafeHandle handle, long faceId, out OcctPlaneGeometry result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_cylinder_geometry(OcctModelingSafeHandle handle, long faceId, out OcctCylinderGeometry result);
+    internal static partial OcctStatus occt_model_face_cylinder_geometry(OcctModelingSafeHandle handle, long faceId, out OcctCylinderGeometry result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_cone_geometry(OcctModelingSafeHandle handle, long faceId, out OcctConeGeometry result);
+    internal static partial OcctStatus occt_model_face_cone_geometry(OcctModelingSafeHandle handle, long faceId, out OcctConeGeometry result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_sphere_geometry(OcctModelingSafeHandle handle, long faceId, out OcctSphereGeometry result);
+    internal static partial OcctStatus occt_model_face_sphere_geometry(OcctModelingSafeHandle handle, long faceId, out OcctSphereGeometry result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_torus_geometry(OcctModelingSafeHandle handle, long faceId, out OcctTorusGeometry result);
+    internal static partial OcctStatus occt_model_face_torus_geometry(OcctModelingSafeHandle handle, long faceId, out OcctTorusGeometry result);
 }
