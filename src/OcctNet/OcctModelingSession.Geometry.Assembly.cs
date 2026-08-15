6c1ddebfdf7e4451749e4858ac84e25e3b012e08
zhangly1403 <zhangly1403@qq.com>
2026-08-15 11:24:48 +0800

    refactor(modeling): use ABI5 assembly geometry API

--- src/OcctNet/OcctModelingSession.Geometry.Assembly.cs
+++ src/OcctNet/OcctModelingSession.Geometry.Assembly.cs
@@ -1,29 +1,37 @@
-﻿namespace OcctNet;
+namespace OcctNet;
 
 public sealed partial class OcctModelingSession
 {
     public OcctModelShape MakeCompound(IEnumerable<OcctModelShape> shapes)
     {
         var ids = ShapeIds(shapes);
-        return CheckShape(ModelNativeMethods.occt_model_make_compound(NativeHandle, ids, ids.Length));
+        var status = ModelNativeMethods.occt_model_assembly_compound_create(
+            NativeHandle, ids, ids.Length, out var result);
+        return CheckShape(status, result);
     }
 
     public OcctModelShape MakeWire(IEnumerable<OcctModelShape> edges)
     {
         var ids = ShapeIds(edges);
-        return CheckShape(ModelNativeMethods.occt_model_make_wire(NativeHandle, ids, ids.Length));
+        var status = ModelNativeMethods.occt_model_assembly_wire_create(
+            NativeHandle, ids, ids.Length, out var result);
+        return CheckShape(status, result);
     }
 
     public OcctModelShape Sew(IEnumerable<OcctModelShape> shapes, double tolerance = 1e-6)
     {
         var ids = ShapeIds(shapes);
         OcctGuard.Positive(tolerance, nameof(tolerance));
-        return CheckShape(ModelNativeMethods.occt_model_sew(NativeHandle, ids, ids.Length, tolerance));
+        var status = ModelNativeMethods.occt_model_assembly_sew(
+            NativeHandle, ids, ids.Length, tolerance, out var result);
+        return CheckShape(status, result);
     }
 
     public OcctModelShape MakeSolidFromShell(OcctModelShape shell)
     {
         EnsureShape(shell);
-        return CheckShape(ModelNativeMethods.occt_model_make_solid_from_shell(_handle, shell.Id));
+        var status = ModelNativeMethods.occt_model_assembly_solid_from_shell_create(
+            _handle, shell.Id, out var result);
+        return CheckShape(status, result);
     }
 }
