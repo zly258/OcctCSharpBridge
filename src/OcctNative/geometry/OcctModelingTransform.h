2ba50a5d71c793fc97aca1501dbc38a8909e2cd4
zhangly1403 <zhangly1403@qq.com>
2026-08-15 11:22:36 +0800

    refactor(modeling): add ABI5 transform API

---
+++ src/OcctNative/geometry/OcctModelingTransform.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "OcctNative.h"
+
+extern "C"
+{
+    OCCTBRIDGE_API OcctStatus occt_model_transform_translate(
+        OcctModelingSessionHandle handle,
+        OcctObjectId shapeId,
+        OcctVector3d vector,
+        OcctObjectId* result);
+
+    OCCTBRIDGE_API OcctStatus occt_model_transform_rotate(
+        OcctModelingSessionHandle handle,
+        OcctObjectId shapeId,
+        OcctPoint3d axisPoint,
+        OcctVector3d axisDirection,
+        double angleDegrees,
+        OcctObjectId* result);
+
+    OCCTBRIDGE_API OcctStatus occt_model_transform_scale(
+        OcctModelingSessionHandle handle,
+        OcctObjectId shapeId,
+        OcctPoint3d center,
+        double factor,
+        OcctObjectId* result);
+
+    OCCTBRIDGE_API OcctStatus occt_model_transform_mirror_plane(
+        OcctModelingSessionHandle handle,
+        OcctObjectId shapeId,
+        OcctPoint3d planePoint,
+        OcctVector3d planeNormal,
+        OcctObjectId* result);
+}
