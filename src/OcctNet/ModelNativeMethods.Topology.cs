0bac2820415cedef5b9f341710e51b63c55d099e
zhangly1403 <zhangly1403@qq.com>
2026-08-15 12:01:49 +0800

    Migrate topology interop to LibraryImport

--- src/OcctNet/ModelNativeMethods.Topology.cs
+++ src/OcctNet/ModelNativeMethods.Topology.cs
@@ -1,18 +1,39 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
 
 namespace OcctNet;
 
 internal static partial class ModelNativeMethods
 {
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_subshapes_copy(OcctModelingSafeHandle handle, long shapeId, int shapeType, [Out] long[]? results, int capacity);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_subshapes_copy(
+        OcctModelingSafeHandle handle,
+        long shapeId,
+        int shapeType,
+        [Out, MarshalUsing(CountElementName = nameof(capacity))] long[]? results,
+        int capacity);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern long occt_model_outer_wire(OcctModelingSafeHandle handle, long faceId);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial long occt_model_outer_wire(OcctModelingSafeHandle handle, long faceId);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_inner_wires_copy(OcctModelingSafeHandle handle, long faceId, [Out] long[]? results, int capacity);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_inner_wires_copy(
+        OcctModelingSafeHandle handle,
+        long faceId,
+        [Out, MarshalUsing(CountElementName = nameof(capacity))] long[]? results,
+        int capacity);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_ancestors_copy(OcctModelingSafeHandle handle, long rootId, long childId, int ancestorType, [Out] long[]? results, int capacity);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_ancestors_copy(
+        OcctModelingSafeHandle handle,
+        long rootId,
+        long childId,
+        int ancestorType,
+        [Out, MarshalUsing(CountElementName = nameof(capacity))] long[]? results,
+        int capacity);
 }
