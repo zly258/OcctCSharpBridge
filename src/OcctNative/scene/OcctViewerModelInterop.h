b3b9aef892b632ae92084832880df0334a31a0c5
zhangly1403 <zhangly1403@qq.com>
2026-08-15 13:03:15 +0800

    Use ABI5 modeling session handle for viewer model interop

--- src/OcctNative/scene/OcctViewerModelInterop.h
+++ src/OcctNative/scene/OcctViewerModelInterop.h
@@ -6,7 +6,7 @@ extern "C"
 {
     OCCTBRIDGE_API OcctStatus occt_engine_object_shape_update_from_model(
         OcctEngineHandle engineHandle,
-        OcctModelHandle modelHandle,
+        OcctModelingSessionHandle modelHandle,
         OcctObjectId viewerObjectId,
         OcctObjectId modelShapeId,
         std::uint32_t options);
