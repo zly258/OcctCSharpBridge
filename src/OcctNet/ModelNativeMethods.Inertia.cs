0c7cfa75356d2e682b76b28817d39e6b79f38bec
zhangly1403 <zhangly1403@qq.com>
2026-08-15 12:00:15 +0800

    Migrate inertia interop to LibraryImport

--- src/OcctNet/ModelNativeMethods.Inertia.cs
+++ src/OcctNet/ModelNativeMethods.Inertia.cs
@@ -1,15 +1,19 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace OcctNet;
 
 internal static partial class ModelNativeMethods
 {
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_shape_linear_inertia(OcctModelingSafeHandle handle, long shapeId, out NativeModelInertiaProperties result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_shape_linear_inertia(OcctModelingSafeHandle handle, long shapeId, out NativeModelInertiaProperties result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_shape_surface_inertia(OcctModelingSafeHandle handle, long shapeId, out NativeModelInertiaProperties result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_shape_surface_inertia(OcctModelingSafeHandle handle, long shapeId, out NativeModelInertiaProperties result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_shape_volume_inertia(OcctModelingSafeHandle handle, long shapeId, out NativeModelInertiaProperties result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_shape_volume_inertia(OcctModelingSafeHandle handle, long shapeId, out NativeModelInertiaProperties result);
 }
