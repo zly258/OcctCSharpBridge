59c8f20d7a0d7134a0b5ae3a96040644119409bf
zhangly1403 <zhangly1403@qq.com>
2026-08-15 14:12:39 +0800

    Migrate B-Spline queries to ABI5 status contract

--- src/OcctNet/ModelNativeMethods.BSpline.cs
+++ src/OcctNet/ModelNativeMethods.BSpline.cs
@@ -7,61 +7,29 @@ internal static partial class ModelNativeMethods
 {
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_bspline_info(
-        OcctModelingSafeHandle handle,
-        long edgeId,
-        out OcctModelBSplineCurveInfoNative result);
+    internal static partial OcctStatus occt_model_edge_bspline_info(OcctModelingSafeHandle handle, long edgeId, out OcctModelBSplineCurveInfoNative result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_bspline_pole_at(
-        OcctModelingSafeHandle handle,
-        long edgeId,
-        int index,
-        out OcctPoint3d pole,
-        out double weight);
+    internal static partial OcctStatus occt_model_edge_bspline_pole_at(OcctModelingSafeHandle handle, long edgeId, int index, out OcctPoint3d pole, out double weight);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_bspline_knot_at(
-        OcctModelingSafeHandle handle,
-        long edgeId,
-        int index,
-        out double knot,
-        out int multiplicity);
+    internal static partial OcctStatus occt_model_edge_bspline_knot_at(OcctModelingSafeHandle handle, long edgeId, int index, out double knot, out int multiplicity);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_bspline_info(
-        OcctModelingSafeHandle handle,
-        long faceId,
-        out OcctModelBSplineSurfaceInfoNative result);
+    internal static partial OcctStatus occt_model_face_bspline_info(OcctModelingSafeHandle handle, long faceId, out OcctModelBSplineSurfaceInfoNative result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_bspline_pole_at(
-        OcctModelingSafeHandle handle,
-        long faceId,
-        int uIndex,
-        int vIndex,
-        out OcctPoint3d pole,
-        out double weight);
+    internal static partial OcctStatus occt_model_face_bspline_pole_at(OcctModelingSafeHandle handle, long faceId, int uIndex, int vIndex, out OcctPoint3d pole, out double weight);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_bspline_u_knot_at(
-        OcctModelingSafeHandle handle,
-        long faceId,
-        int index,
-        out double knot,
-        out int multiplicity);
+    internal static partial OcctStatus occt_model_face_bspline_u_knot_at(OcctModelingSafeHandle handle, long faceId, int index, out double knot, out int multiplicity);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_bspline_v_knot_at(
-        OcctModelingSafeHandle handle,
-        long faceId,
-        int index,
-        out double knot,
-        out int multiplicity);
+    internal static partial OcctStatus occt_model_face_bspline_v_knot_at(OcctModelingSafeHandle handle, long faceId, int index, out double knot, out int multiplicity);
 }
