9c2afdc620856e11841a08b377da5f9fcaa86b86
zhangly1403 <zhangly1403@qq.com>
2026-08-15 11:24:09 +0800

    refactor(modeling): bind ABI5 planar geometry API

--- src/OcctNet/ModelNativeMethods.Geometry.Planar.cs
+++ src/OcctNet/ModelNativeMethods.Geometry.Planar.cs
@@ -1,11 +1,49 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace OcctNet;
 
 internal static partial class ModelNativeMethods
 {
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_regular_polygon(OcctModelingSafeHandle handle, OcctPoint3d center, OcctVector3d normal, OcctVector3d xDirection, double radius, int sideCount, int makeFace);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_rectangle_wire(OcctModelingSafeHandle handle, OcctPoint3d origin, OcctVector3d xDirection, OcctVector3d normal, double width, double height);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_plane_face(OcctModelingSafeHandle handle, OcctPoint3d origin, OcctVector3d xDirection, OcctVector3d normal, double width, double height);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_face_from_wire(OcctModelingSafeHandle handle, long wireId, int onlyPlane);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_planar_regular_polygon_create(
+        OcctModelingSafeHandle handle,
+        OcctPoint3d center,
+        OcctVector3d normal,
+        OcctVector3d xDirection,
+        double radius,
+        int sideCount,
+        int makeFace,
+        out long result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_planar_rectangle_wire_create(
+        OcctModelingSafeHandle handle,
+        OcctPoint3d origin,
+        OcctVector3d xDirection,
+        OcctVector3d normal,
+        double width,
+        double height,
+        out long result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_planar_face_create(
+        OcctModelingSafeHandle handle,
+        OcctPoint3d origin,
+        OcctVector3d xDirection,
+        OcctVector3d normal,
+        double width,
+        double height,
+        out long result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_planar_face_from_wire_create(
+        OcctModelingSafeHandle handle,
+        long wireId,
+        int onlyPlane,
+        out long result);
 }
