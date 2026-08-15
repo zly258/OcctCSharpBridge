33239330e6c9cd12f63350276c2e3ebb361db5fc
zhangly1403 <zhangly1403@qq.com>
2026-08-15 11:08:22 +0800

    Move modeling primitive bindings to ABI5

--- src/OcctNet/ModelNativeMethods.Geometry.Primitives.cs
+++ src/OcctNet/ModelNativeMethods.Geometry.Primitives.cs
@@ -1,13 +1,31 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace OcctNet;
 
 internal static partial class ModelNativeMethods
 {
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_box(OcctModelingSafeHandle handle, double x, double y, double z, double dx, double dy, double dz);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_cylinder(OcctModelingSafeHandle handle, OcctPoint3d origin, OcctVector3d axis, double radius, double height);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_cone(OcctModelingSafeHandle handle, OcctPoint3d origin, OcctVector3d axis, double radius1, double radius2, double height);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_sphere(OcctModelingSafeHandle handle, OcctPoint3d center, double radius);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_torus(OcctModelingSafeHandle handle, OcctPoint3d center, OcctVector3d axis, double majorRadius, double minorRadius);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern long occt_model_make_wedge(OcctModelingSafeHandle handle, double dx, double dy, double dz, double ltx);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_make_box(OcctModelingSafeHandle handle, double x, double y, double z, double dx, double dy, double dz, out long result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_make_cylinder(OcctModelingSafeHandle handle, OcctPoint3d origin, OcctVector3d axis, double radius, double height, out long result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_make_cone(OcctModelingSafeHandle handle, OcctPoint3d origin, OcctVector3d axis, double radius1, double radius2, double height, out long result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_make_sphere(OcctModelingSafeHandle handle, OcctPoint3d center, double radius, out long result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_make_torus(OcctModelingSafeHandle handle, OcctPoint3d center, OcctVector3d axis, double majorRadius, double minorRadius, out long result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_make_wedge(OcctModelingSafeHandle handle, double dx, double dy, double dz, double ltx, out long result);
 }
