9837b014155d9d677dbe6f5162448c7e49fc5430
zhangly1403 <zhangly1403@qq.com>
2026-08-15 14:10:47 +0800

    Migrate differential geometry queries to ABI5 status contract

--- src/OcctNet/ModelNativeMethods.DifferentialGeometry.cs
+++ src/OcctNet/ModelNativeMethods.DifferentialGeometry.cs
@@ -7,25 +7,25 @@ internal static partial class ModelNativeMethods
 {
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_parameter_range(OcctModelingSafeHandle handle, long edgeId, out NativeModelParameterRange result);
+    internal static partial OcctStatus occt_model_edge_parameter_range(OcctModelingSafeHandle handle, long edgeId, out NativeModelParameterRange result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_differential(OcctModelingSafeHandle handle, long edgeId, double parameter, out NativeModelCurveDifferential result);
+    internal static partial OcctStatus occt_model_edge_differential(OcctModelingSafeHandle handle, long edgeId, double parameter, out NativeModelCurveDifferential result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_edge_curvature(OcctModelingSafeHandle handle, long edgeId, double parameter, double resolution, out NativeModelCurveCurvature result);
+    internal static partial OcctStatus occt_model_edge_curvature(OcctModelingSafeHandle handle, long edgeId, double parameter, double resolution, out NativeModelCurveCurvature result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_periodicity(OcctModelingSafeHandle handle, long faceId, out NativeModelSurfacePeriodicity result);
+    internal static partial OcctStatus occt_model_face_periodicity(OcctModelingSafeHandle handle, long faceId, out NativeModelSurfacePeriodicity result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_differential(OcctModelingSafeHandle handle, long faceId, double u, double v, double resolution, out NativeModelSurfaceDifferential result);
+    internal static partial OcctStatus occt_model_face_differential(OcctModelingSafeHandle handle, long faceId, double u, double v, double resolution, out NativeModelSurfaceDifferential result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_face_curvature(OcctModelingSafeHandle handle, long faceId, double u, double v, double resolution, out NativeModelSurfaceCurvature result);
+    internal static partial OcctStatus occt_model_face_curvature(OcctModelingSafeHandle handle, long faceId, double u, double v, double resolution, out NativeModelSurfaceCurvature result);
 }
