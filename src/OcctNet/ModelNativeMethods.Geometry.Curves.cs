46d4dc328b9f79db47fb87382c2f775145fa67ca
zhangly1403 <zhangly1403@qq.com>
2026-08-15 11:28:24 +0800

    fix(modeling): bind collision-free ABI5 curve exports

--- src/OcctNet/ModelNativeMethods.Geometry.Curves.cs
+++ src/OcctNet/ModelNativeMethods.Geometry.Curves.cs
@@ -7,37 +7,83 @@ internal static partial class ModelNativeMethods
 {
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial OcctStatus occt_model_make_vertex(OcctModelingSafeHandle handle, OcctPoint3d point, out long result);
+    internal static partial OcctStatus occt_model_curve_vertex_create(
+        OcctModelingSafeHandle handle,
+        OcctPoint3d point,
+        out long result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial OcctStatus occt_model_make_line(OcctModelingSafeHandle handle, OcctPoint3d start, OcctPoint3d end, out long result);
+    internal static partial OcctStatus occt_model_curve_line_create(
+        OcctModelingSafeHandle handle,
+        OcctPoint3d start,
+        OcctPoint3d end,
+        out long result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial OcctStatus occt_model_make_polyline(OcctModelingSafeHandle handle, [In] OcctPoint3d[] points, int count, int closed, out long result);
+    internal static partial OcctStatus occt_model_curve_polyline_create(
+        OcctModelingSafeHandle handle,
+        [In] OcctPoint3d[] points,
+        int count,
+        int closed,
+        out long result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial OcctStatus occt_model_make_circle(OcctModelingSafeHandle handle, OcctPoint3d center, OcctVector3d normal, double radius, out long result);
+    internal static partial OcctStatus occt_model_curve_circle_create(
+        OcctModelingSafeHandle handle,
+        OcctPoint3d center,
+        OcctVector3d normal,
+        double radius,
+        out long result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial OcctStatus occt_model_make_arc_three_points(OcctModelingSafeHandle handle, OcctPoint3d start, OcctPoint3d middle, OcctPoint3d end, out long result);
+    internal static partial OcctStatus occt_model_curve_arc_three_points_create(
+        OcctModelingSafeHandle handle,
+        OcctPoint3d start,
+        OcctPoint3d middle,
+        OcctPoint3d end,
+        out long result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial OcctStatus occt_model_make_arc_center(OcctModelingSafeHandle handle, OcctPoint3d center, OcctVector3d normal, OcctVector3d xDirection, double radius, double startAngleDegrees, double endAngleDegrees, out long result);
+    internal static partial OcctStatus occt_model_curve_arc_center_create(
+        OcctModelingSafeHandle handle,
+        OcctPoint3d center,
+        OcctVector3d normal,
+        OcctVector3d xDirection,
+        double radius,
+        double startAngleDegrees,
+        double endAngleDegrees,
+        out long result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial OcctStatus occt_model_make_ellipse(OcctModelingSafeHandle handle, OcctPoint3d center, OcctVector3d normal, double majorRadius, double minorRadius, out long result);
+    internal static partial OcctStatus occt_model_curve_ellipse_create(
+        OcctModelingSafeHandle handle,
+        OcctPoint3d center,
+        OcctVector3d normal,
+        double majorRadius,
+        double minorRadius,
+        out long result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial OcctStatus occt_model_make_bezier(OcctModelingSafeHandle handle, [In] OcctPoint3d[] poles, int count, out long result);
+    internal static partial OcctStatus occt_model_curve_bezier_create(
+        OcctModelingSafeHandle handle,
+        [In] OcctPoint3d[] poles,
+        int count,
+        out long result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial OcctStatus occt_model_make_bspline_interpolated(OcctModelingSafeHandle handle, [In] OcctPoint3d[] points, int count, int periodic, double tolerance, out long result);
+    internal static partial OcctStatus occt_model_curve_bspline_interpolated_create(
+        OcctModelingSafeHandle handle,
+        [In] OcctPoint3d[] points,
+        int count,
+        int periodic,
+        double tolerance,
+        out long result);
 }
