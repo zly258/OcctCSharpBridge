92070207547daa37045339f8a10addca1eed9fa7
zhangly1403 <zhangly1403@qq.com>
2026-08-15 12:01:40 +0800

    Migrate modeling history interop to LibraryImport

--- src/OcctNet/ModelNativeMethods.History.cs
+++ src/OcctNet/ModelNativeMethods.History.cs
@@ -1,13 +1,41 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
 
 namespace OcctNet;
 
 internal static partial class ModelNativeMethods
 {
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern int occt_model_history_generated_copy(OcctModelingSafeHandle handle, long operationId, long sourceShapeId, [Out] long[]? results, int capacity);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern int occt_model_history_modified_copy(OcctModelingSafeHandle handle, long operationId, long sourceShapeId, [Out] long[]? results, int capacity);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern int occt_model_history_is_removed(OcctModelingSafeHandle handle, long operationId, long sourceShapeId);
     [LibraryImport(LibraryName)]
-    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
-    internal static partial OcctStatus occt_model_history_summary(OcctModelingSafeHandle handle, long operationId, long sourceShapeId, out NativeModelTopologyHistorySummary result);
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_history_generated_copy(
+        OcctModelingSafeHandle handle,
+        long operationId,
+        long sourceShapeId,
+        [Out, MarshalUsing(CountElementName = nameof(capacity))] long[]? results,
+        int capacity);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_history_modified_copy(
+        OcctModelingSafeHandle handle,
+        long operationId,
+        long sourceShapeId,
+        [Out, MarshalUsing(CountElementName = nameof(capacity))] long[]? results,
+        int capacity);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_history_is_removed(
+        OcctModelingSafeHandle handle,
+        long operationId,
+        long sourceShapeId);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_history_summary(
+        OcctModelingSafeHandle handle,
+        long operationId,
+        long sourceShapeId,
+        out NativeModelTopologyHistorySummary result);
 }
