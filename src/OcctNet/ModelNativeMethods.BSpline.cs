cb51f14012fb2c201f9be33172ec98757c7955e4
zhangly1403 <zhangly1403@qq.com>
2026-08-15 12:02:28 +0800

    Migrate BSpline interop to LibraryImport

--- src/OcctNet/ModelNativeMethods.BSpline.cs
+++ src/OcctNet/ModelNativeMethods.BSpline.cs
@@ -1,39 +1,45 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace OcctNet;
 
 internal static partial class ModelNativeMethods
 {
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_edge_bspline_info(
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_edge_bspline_info(
         OcctModelingSafeHandle handle,
         long edgeId,
         out OcctModelBSplineCurveInfoNative result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_edge_bspline_pole_at(
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_edge_bspline_pole_at(
         OcctModelingSafeHandle handle,
         long edgeId,
         int index,
         out OcctPoint3d pole,
         out double weight);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_edge_bspline_knot_at(
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_edge_bspline_knot_at(
         OcctModelingSafeHandle handle,
         long edgeId,
         int index,
         out double knot,
         out int multiplicity);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_bspline_info(
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_bspline_info(
         OcctModelingSafeHandle handle,
         long faceId,
         out OcctModelBSplineSurfaceInfoNative result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_bspline_pole_at(
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_bspline_pole_at(
         OcctModelingSafeHandle handle,
         long faceId,
         int uIndex,
@@ -41,16 +47,18 @@ internal static partial class ModelNativeMethods
         out OcctPoint3d pole,
         out double weight);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_bspline_u_knot_at(
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_bspline_u_knot_at(
         OcctModelingSafeHandle handle,
         long faceId,
         int index,
         out double knot,
         out int multiplicity);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_bspline_v_knot_at(
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_bspline_v_knot_at(
         OcctModelingSafeHandle handle,
         long faceId,
         int index,
