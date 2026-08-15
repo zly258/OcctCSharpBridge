23192fa1cd785b2ced42f56b155fdddb8f7d1b52
zhangly1403 <zhangly1403@qq.com>
2026-08-15 08:46:01 +0800

    Replace legacy manipulator DllImports with ABI5 LibraryImport bindings

--- src/OcctNet/ManipulatorNativeMethods.cs
+++ src/OcctNet/ManipulatorNativeMethods.cs
@@ -1,89 +1,137 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace OcctNet;
 
-internal static class ManipulatorNativeMethods
+[StructLayout(LayoutKind.Sequential)]
+internal struct NativeManipulatorAttachOptionsV1
+{
+    internal uint StructSize;
+    internal uint ApiVersion;
+    internal int AdjustPosition;
+    internal int AdjustSize;
+    internal int EnableModes;
+}
+
+[Flags]
+internal enum NativeManipulatorUpdateMask : uint
+{
+    Part = 1u << 0,
+    ModeEnabled = 1u << 1,
+    ModeActivationOnDetection = 1u << 2,
+    Position = 1u << 3,
+    Size = 1u << 4,
+    Gap = 1u << 5,
+    ZoomPersistence = 1u << 6,
+    Skin = 1u << 7
+}
+
+[StructLayout(LayoutKind.Sequential)]
+internal struct NativeManipulatorUpdateOptions
+{
+    internal uint StructSize;
+    internal uint ApiVersion;
+    internal NativeManipulatorUpdateMask UpdateMask;
+    internal int AxisIndex;
+    internal int Mode;
+    internal int Enabled;
+    internal OcctPoint3d Origin;
+    internal OcctVector3d Normal;
+    internal OcctVector3d XDirection;
+    internal double Size;
+    internal double Gap;
+    internal int SkinMode;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+internal struct NativeManipulatorStateV1
+{
+    internal uint StructSize;
+    internal uint ApiVersion;
+    internal int Attached;
+    internal int ActiveMode;
+    internal int ActiveAxisIndex;
+    internal int HasActiveTransformation;
+    internal int ModeActivationOnDetection;
+    internal int ZoomPersistence;
+    internal int SkinMode;
+    internal OcctPoint3d Origin;
+    internal OcctVector3d Normal;
+    internal OcctVector3d XDirection;
+    internal double Size;
+}
+
+internal enum NativeManipulatorTransformAction
+{
+    Start = 0,
+    Update = 1,
+    Stop = 2,
+    DeactivateMode = 3
+}
+
+[StructLayout(LayoutKind.Sequential)]
+internal struct NativeManipulatorTransformOptions
+{
+    internal uint StructSize;
+    internal uint ApiVersion;
+    internal NativeManipulatorTransformAction Action;
+    internal int X;
+    internal int Y;
+    internal int Apply;
+}
+
+internal static partial class ManipulatorNativeMethods
 {
     private const string LibraryName = "OcctNative";
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern long occt_add_manipulator(OcctEngineSafeHandle handle);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_engine_manipulator_create(
+        OcctEngineSafeHandle handle,
+        out long manipulatorId);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_attach_manipulator(
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_engine_manipulator_attach(
         OcctEngineSafeHandle handle,
         long manipulatorId,
-        [In] long[] objectIds,
+        IntPtr objectIds,
         int count,
-        in NativeOcctManipulatorAttachOptions options);
+        in NativeManipulatorAttachOptionsV1 options);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_detach_manipulator(OcctEngineSafeHandle handle, long manipulatorId);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_engine_manipulator_detach(
+        OcctEngineSafeHandle handle,
+        long manipulatorId);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_set_manipulator_part(
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_engine_manipulator_update(
         OcctEngineSafeHandle handle,
         long manipulatorId,
-        int axisIndex,
-        int mode,
-        int enabled);
+        in NativeManipulatorUpdateOptions options);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_set_manipulator_mode_enabled(
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_engine_manipulator_state_get(
         OcctEngineSafeHandle handle,
         long manipulatorId,
-        int mode,
-        int enabled);
+        out NativeManipulatorStateV1 result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_set_manipulator_mode_activation_on_detection(
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_engine_manipulator_targets_get(
         OcctEngineSafeHandle handle,
         long manipulatorId,
-        int enabled);
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_set_manipulator_position(
-        OcctEngineSafeHandle handle,
-        long manipulatorId,
-        OcctPoint3d origin,
-        OcctVector3d normal,
-        OcctVector3d xDirection);
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_set_manipulator_size(OcctEngineSafeHandle handle, long manipulatorId, double size);
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_set_manipulator_gap(OcctEngineSafeHandle handle, long manipulatorId, double gap);
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_set_manipulator_zoom_persistence(OcctEngineSafeHandle handle, long manipulatorId, int enabled);
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_set_manipulator_skin(OcctEngineSafeHandle handle, long manipulatorId, int skinMode);
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_get_manipulator_state(
-        OcctEngineSafeHandle handle,
-        long manipulatorId,
-        out NativeOcctManipulatorState result);
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_get_manipulator_objects(
-        OcctEngineSafeHandle handle,
-        long manipulatorId,
-        [Out] long[]? objectIds,
+        IntPtr objectIds,
         int capacity,
         out int count);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_start_manipulator_transform(OcctEngineSafeHandle handle, long manipulatorId, int x, int y);
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_update_manipulator_transform(OcctEngineSafeHandle handle, long manipulatorId, int x, int y);
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_stop_manipulator_transform(OcctEngineSafeHandle handle, long manipulatorId, int apply);
-
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_deactivate_manipulator_mode(OcctEngineSafeHandle handle, long manipulatorId);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_engine_manipulator_transform(
+        OcctEngineSafeHandle handle,
+        long manipulatorId,
+        in NativeManipulatorTransformOptions options);
 }
