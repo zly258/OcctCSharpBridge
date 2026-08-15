66d0503659157b90b9914da73e55d2f95fe82caa
zhangly1403 <zhangly1403@qq.com>
2026-08-15 11:59:37 +0800

    Migrate differential geometry interop to LibraryImport

--- src/OcctNet/ModelNativeMethods.DifferentialGeometry.cs
+++ src/OcctNet/ModelNativeMethods.DifferentialGeometry.cs
@@ -1,24 +1,31 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace OcctNet;
 
 internal static partial class ModelNativeMethods
 {
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_edge_parameter_range(OcctModelingSafeHandle handle, long edgeId, out NativeModelParameterRange result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_edge_parameter_range(OcctModelingSafeHandle handle, long edgeId, out NativeModelParameterRange result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_edge_differential(OcctModelingSafeHandle handle, long edgeId, double parameter, out NativeModelCurveDifferential result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_edge_differential(OcctModelingSafeHandle handle, long edgeId, double parameter, out NativeModelCurveDifferential result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_edge_curvature(OcctModelingSafeHandle handle, long edgeId, double parameter, double resolution, out NativeModelCurveCurvature result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_edge_curvature(OcctModelingSafeHandle handle, long edgeId, double parameter, double resolution, out NativeModelCurveCurvature result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_periodicity(OcctModelingSafeHandle handle, long faceId, out NativeModelSurfacePeriodicity result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_periodicity(OcctModelingSafeHandle handle, long faceId, out NativeModelSurfacePeriodicity result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_differential(OcctModelingSafeHandle handle, long faceId, double u, double v, double resolution, out NativeModelSurfaceDifferential result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_differential(OcctModelingSafeHandle handle, long faceId, double u, double v, double resolution, out NativeModelSurfaceDifferential result);
 
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern int occt_model_face_curvature(OcctModelingSafeHandle handle, long faceId, double u, double v, double resolution, out NativeModelSurfaceCurvature result);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int occt_model_face_curvature(OcctModelingSafeHandle handle, long faceId, double u, double v, double resolution, out NativeModelSurfaceCurvature result);
 }
