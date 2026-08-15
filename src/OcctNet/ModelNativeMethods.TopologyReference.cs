d8b19d380125cdc544e354151db4fe9e0915bfa6
zhangly1403 <zhangly1403@qq.com>
2026-08-15 13:59:22 +0800

    Migrate topology reference API to ABI5 status contract

--- src/OcctNet/ModelNativeMethods.TopologyReference.cs
+++ src/OcctNet/ModelNativeMethods.TopologyReference.cs
@@ -7,7 +7,7 @@ internal static partial class ModelNativeMethods
 {
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_create_topology_reference(
+    internal static partial OcctStatus occt_model_create_topology_reference(
         OcctModelingSafeHandle handle,
         long rootShapeId,
         long subshapeId,
@@ -15,7 +15,7 @@ internal static partial class ModelNativeMethods
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_resolve_topology_reference(
+    internal static partial OcctStatus occt_model_resolve_topology_reference(
         OcctModelingSafeHandle handle,
         long rootShapeId,
         in NativeModelTopologyReference reference,
@@ -24,7 +24,7 @@ internal static partial class ModelNativeMethods
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_resolve_topology_reference_with_history(
+    internal static partial OcctStatus occt_model_resolve_topology_reference_with_history(
         OcctModelingSafeHandle handle,
         long rootShapeId,
         long operationId,
