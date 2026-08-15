860fcaade7e56a318dad746825b906e2f13bd6b2
zhangly1403 <zhangly1403@qq.com>
2026-08-15 14:05:48 +0800

    Align geometry queries with ABI5 status results

--- src/OcctNet/OcctModelingSession.GeometryQueries.cs
+++ src/OcctNet/OcctModelingSession.GeometryQueries.cs
@@ -5,14 +5,14 @@ public sealed partial class OcctModelingSession
     public OcctPoint3d GetVertexPoint(OcctModelShape vertex)
     {
         EnsureShape(vertex);
-        Check(ModelNativeMethods.occt_model_vertex_point(_handle, vertex.Id, out var result));
+        CheckStatus(ModelNativeMethods.occt_model_vertex_point(_handle, vertex.Id, out var result));
         return result;
     }
 
     public (OcctPoint3d Start, OcctPoint3d End) GetEdgeEndpoints(OcctModelShape edge)
     {
         EnsureShape(edge);
-        Check(ModelNativeMethods.occt_model_edge_endpoints(
+        CheckStatus(ModelNativeMethods.occt_model_edge_endpoints(
             _handle,
             edge.Id,
             out var start,
@@ -26,7 +26,7 @@ public sealed partial class OcctModelingSession
     {
         EnsureShape(edge);
         OcctGuard.UnitInterval(normalizedParameter, nameof(normalizedParameter));
-        Check(ModelNativeMethods.occt_model_edge_point_at(
+        CheckStatus(ModelNativeMethods.occt_model_edge_point_at(
             _handle,
             edge.Id,
             normalizedParameter,
@@ -38,19 +38,21 @@ public sealed partial class OcctModelingSession
     public OcctCurveType GetEdgeCurveType(OcctModelShape edge)
     {
         EnsureShape(edge);
-        return (OcctCurveType)ModelNativeMethods.occt_model_edge_curve_type(_handle, edge.Id);
+        CheckStatus(ModelNativeMethods.occt_model_edge_curve_type(_handle, edge.Id, out var result));
+        return result;
     }
 
     public OcctSurfaceType GetFaceSurfaceType(OcctModelShape face)
     {
         EnsureShape(face);
-        return (OcctSurfaceType)ModelNativeMethods.occt_model_face_surface_type(_handle, face.Id);
+        CheckStatus(ModelNativeMethods.occt_model_face_surface_type(_handle, face.Id, out var result));
+        return result;
     }
 
     public OcctUvBounds GetFaceUvBounds(OcctModelShape face)
     {
         EnsureShape(face);
-        Check(ModelNativeMethods.occt_model_face_uv_bounds(_handle, face.Id, out var result));
+        CheckStatus(ModelNativeMethods.occt_model_face_uv_bounds(_handle, face.Id, out var result));
         return result;
     }
 
@@ -59,7 +61,7 @@ public sealed partial class OcctModelingSession
         EnsureShape(face);
         OcctGuard.Finite(u, nameof(u));
         OcctGuard.Finite(v, nameof(v));
-        Check(ModelNativeMethods.occt_model_face_point_normal(
+        CheckStatus(ModelNativeMethods.occt_model_face_point_normal(
             _handle,
             face.Id,
             u,
