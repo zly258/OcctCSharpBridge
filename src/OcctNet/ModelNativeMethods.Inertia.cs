78bd2609808b3563a6f6337e7235dea4622757e4
zhangly1403 <zhangly1403@qq.com>
2026-08-15 13:54:24 +0800

    Migrate inertia queries to ABI5 status contract

--- src/OcctNet/ModelNativeMethods.Inertia.cs
+++ src/OcctNet/ModelNativeMethods.Inertia.cs
@@ -7,13 +7,22 @@ internal static partial class ModelNativeMethods
 {
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_shape_linear_inertia(OcctModelingSafeHandle handle, long shapeId, out NativeModelInertiaProperties result);
+    internal static partial OcctStatus occt_model_shape_linear_inertia(
+        OcctModelingSafeHandle handle,
+        long shapeId,
+        out NativeModelInertiaProperties result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_shape_surface_inertia(OcctModelingSafeHandle handle, long shapeId, out NativeModelInertiaProperties result);
+    internal static partial OcctStatus occt_model_shape_surface_inertia(
+        OcctModelingSafeHandle handle,
+        long shapeId,
+        out NativeModelInertiaProperties result);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    internal static partial int occt_model_shape_volume_inertia(OcctModelingSafeHandle handle, long shapeId, out NativeModelInertiaProperties result);
+    internal static partial OcctStatus occt_model_shape_volume_inertia(
+        OcctModelingSafeHandle handle,
+        long shapeId,
+        out NativeModelInertiaProperties result);
 }
