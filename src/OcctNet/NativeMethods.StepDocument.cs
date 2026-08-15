8bcd58bc11d6fc95f8179364ba2c49aa9b997863
zhangly1403 <zhangly1403@qq.com>
2026-08-15 10:54:35 +0800

    Remove legacy STEP document PInvoke binding

--- src/OcctNet/NativeMethods.StepDocument.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OcctNet;
-
-internal static partial class NativeMethods
-{
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-    internal static extern IntPtr occt_get_last_step_document_json(OcctEngineSafeHandle handle);
-}
