efec3807b66ac580c3bc1201871372d766a4d61a
zhangly1403 <zhangly1403@qq.com>
2026-08-15 11:42:00 +0800

    refactor: migrate boolean interop to LibraryImport ABI5

--- src/OcctNet/ModelNativeMethods.Algorithms.Boolean.cs
+++ src/OcctNet/ModelNativeMethods.Algorithms.Boolean.cs
@@ -1,9 +1,28 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace OcctNet;
 
 internal static partial class ModelNativeMethods
 {
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern NativeModelAlgorithmResult occt_model_boolean(OcctModelingSafeHandle handle, int operation, long leftId, long rightId, in NativeModelBooleanOptions options);
-    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)] internal static extern NativeModelAlgorithmResult occt_model_split(OcctModelingSafeHandle handle, [In] long[] objectIds, int objectCount, [In] long[] toolIds, int toolCount, in NativeModelBooleanOptions options);
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_boolean_execute(
+        OcctModelingSafeHandle handle,
+        int operation,
+        long leftId,
+        long rightId,
+        in NativeModelBooleanOptions options,
+        out NativeModelAlgorithmResult result);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial OcctStatus occt_model_boolean_split_execute(
+        OcctModelingSafeHandle handle,
+        [In] long[] objectIds,
+        int objectCount,
+        [In] long[] toolIds,
+        int toolCount,
+        in NativeModelBooleanOptions options,
+        out NativeModelAlgorithmResult result);
 }
