43de15fada5303b53bbd5def8f933b4bdf666078
zhangly1403 <zhangly1403@qq.com>
2026-08-16 12:58:39 +0800

    Add WinForms native handle lifecycle event

---
+++ src/OcctNet.WinForms/OcctViewportControl.NativeHandle.cs
@@ -0,0 +1,28 @@
+namespace OcctNet;
+
+public sealed partial class OcctViewportControl
+{
+    private IntPtr _nativeHandle;
+
+    public IntPtr NativeHandle => _nativeHandle;
+
+    public event EventHandler<OcctNativeHandleChangedEventArgs>? NativeHandleChanged;
+
+    private void SetNativeHandle(IntPtr nativeHandle, long generation)
+    {
+        if (_nativeHandle == nativeHandle) return;
+
+        var previous = _nativeHandle;
+        _nativeHandle = nativeHandle;
+        try
+        {
+            NativeHandleChanged?.Invoke(
+                this,
+                new OcctNativeHandleChangedEventArgs(previous, nativeHandle, generation));
+        }
+        catch (Exception exception)
+        {
+            ReportLifecycleError(exception);
+        }
+    }
+}
