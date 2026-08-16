f57ccc474f48b0f17fd69e584e67e458acea8733
zhangly1403 <zhangly1403@qq.com>
2026-08-16 12:37:17 +0800

    Relay Avalonia hover changes from core viewer

--- src/OcctNet.Avalonia/OcctAvaloniaViewport.Hover.cs
+++ src/OcctNet.Avalonia/OcctAvaloniaViewport.Hover.cs
@@ -2,29 +2,56 @@ namespace OcctNet;
 
 public sealed partial class OcctAvaloniaViewport
 {
-    private readonly OcctViewportHoverTracker _hoverTracker = new();
+    private EventHandler<OcctViewportHoverHitChangedEventArgs>? _hoverHitChanged;
+    private OcctEngine? _hoverEngine;
+    private bool _hoverLifecycleHooked;
 
-    public event EventHandler<OcctViewportHoverHitChangedEventArgs>? HoverHitChanged;
-
-    private void UpdateHoverHit(int screenX, int screenY)
+    public event EventHandler<OcctViewportHoverHitChangedEventArgs>? HoverHitChanged
     {
-        if (_engine?.IsInitialized != true || HoverHitChanged is null) return;
-
-        OcctSelectionHitDetail? hit = _engine.TryGetDetectedHitDetail(out var detected)
-            ? detected
-            : null;
-        if (!_hoverTracker.Update(hit)) return;
-
-        HoverHitChanged.Invoke(this, new OcctViewportHoverHitChangedEventArgs(screenX, screenY, hit));
+        add
+        {
+            _hoverHitChanged += value;
+            EnsureHoverLifecycleHook();
+            if (_engine is not null) AttachHoverEngine(_engine);
+        }
+        remove
+        {
+            _hoverHitChanged -= value;
+            if (_hoverHitChanged is null) DetachHoverEngine();
+        }
     }
 
-    private void ClearHoverHit(int screenX, int screenY)
+    private void EnsureHoverLifecycleHook()
     {
-        if (!_hoverTracker.Clear() || HoverHitChanged is null) return;
-        TryInvoke(() => HoverHitChanged?.Invoke(
-            this,
-            new OcctViewportHoverHitChangedEventArgs(screenX, screenY, null)));
+        if (_hoverLifecycleHooked) return;
+        _hoverLifecycleHooked = true;
+        EngineRecreated += OnHoverEngineRecreated;
+        EngineDisposing += OnHoverEngineDisposing;
     }
 
-    private void ResetHoverTracking() => _hoverTracker.Clear();
+    private void OnHoverEngineRecreated(object? sender, OcctEngineLifecycleEventArgs e) =>
+        AttachHoverEngine(e.Engine);
+
+    private void OnHoverEngineDisposing(object? sender, OcctEngineLifecycleEventArgs e)
+    {
+        if (ReferenceEquals(_hoverEngine, e.Engine)) DetachHoverEngine();
+    }
+
+    private void AttachHoverEngine(OcctEngine engine)
+    {
+        if (_hoverHitChanged is null || ReferenceEquals(_hoverEngine, engine)) return;
+        DetachHoverEngine();
+        _hoverEngine = engine;
+        _hoverEngine.DetectedHitChanged += RelayHoverHitChanged;
+    }
+
+    private void DetachHoverEngine()
+    {
+        if (_hoverEngine is null) return;
+        _hoverEngine.DetectedHitChanged -= RelayHoverHitChanged;
+        _hoverEngine = null;
+    }
+
+    private void RelayHoverHitChanged(object? sender, OcctViewportHoverHitChangedEventArgs e) =>
+        _hoverHitChanged?.Invoke(this, e);
 }
