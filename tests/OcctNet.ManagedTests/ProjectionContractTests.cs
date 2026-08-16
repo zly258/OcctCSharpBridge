4843bd8843dec5b7563480697e495099ed79feca
zhangly1403 <zhangly1403@qq.com>
2026-08-16 13:32:34 +0800

    Validate projection tangent and normal layouts

--- tests/OcctNet.ManagedTests/ProjectionContractTests.cs
+++ tests/OcctNet.ManagedTests/ProjectionContractTests.cs
@@ -12,27 +12,31 @@ public sealed class ProjectionContractTests
     {
         var actualEdgeSize = Marshal.SizeOf<OcctEdgeProjectionResult>();
         var actualFaceSize = Marshal.SizeOf<OcctFaceProjectionResult>();
-        Assert.AreEqual(40, actualEdgeSize);
-        Assert.AreEqual(48, actualFaceSize);
+        Assert.AreEqual(64, actualEdgeSize);
+        Assert.AreEqual(72, actualFaceSize);
 
         var edge = new OcctEdgeProjectionResult
         {
             Point = new OcctPoint3d(1, 2, 3),
+            Tangent = OcctVector3d.UnitX,
             NormalizedParameter = 0.25,
             Distance = 4
         };
         Assert.AreEqual(new OcctPoint3d(1, 2, 3), edge.Point);
+        Assert.AreEqual(OcctVector3d.UnitX, edge.Tangent);
         Assert.AreEqual(0.25, edge.NormalizedParameter);
         Assert.AreEqual(4.0, edge.Distance);
 
         var face = new OcctFaceProjectionResult
         {
             Point = new OcctPoint3d(5, 6, 7),
+            Normal = OcctVector3d.UnitZ,
             U = 8,
             V = 9,
             Distance = 10
         };
         Assert.AreEqual(new OcctPoint3d(5, 6, 7), face.Point);
+        Assert.AreEqual(OcctVector3d.UnitZ, face.Normal);
         Assert.AreEqual(8.0, face.U);
         Assert.AreEqual(9.0, face.V);
         Assert.AreEqual(10.0, face.Distance);
