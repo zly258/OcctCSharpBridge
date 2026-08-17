8d9a2ccdcb7e6710f5160ae90d2c36b3a2c0d179
zhangly1403 <zhangly1403@qq.com>
2026-08-17 15:38:24 +0800

    Add native ViewCube click contract

--- src/OcctNative/presentation/OcctViewCube.h
+++ src/OcctNative/presentation/OcctViewCube.h
@@ -7,4 +7,12 @@ extern "C"
     OCCTBRIDGE_API OcctStatus occt_engine_view_cube_language_set(
         OcctEngineHandle handle,
         int language);
+
+    // Gives the native AIS_ViewCube first chance to consume a pointer click.
+    // handled is set to 1 only when the detected owner belongs to the current view cube.
+    OCCTBRIDGE_API OcctStatus occt_engine_view_cube_try_click(
+        OcctEngineHandle handle,
+        int x,
+        int y,
+        int* handled);
 }
