b0ba46360316b4af42fe19e2dba6098f07481073
zhangly1403 <zhangly1403@qq.com>
2026-08-18 08:45:13 +0800

    Cover explicit-plane viewer annotation API

---
+++ tests/OcctNet.ManagedTests/ViewerAnnotationContractTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OcctNet;
+
+namespace OcctNet.ManagedTests;
+
+[TestClass]
+public sealed class ViewerAnnotationContractTests
+{
+    [TestMethod]
+    public void LengthDimensionSupportsHostOwnedDraftingPlane()
+    {
+        var overload = typeof(OcctEngine)
+            .GetMethods()
+            .SingleOrDefault(method =>
+            {
+                if (!string.Equals(method.Name, nameof(OcctEngine.AddLengthDimension), StringComparison.Ordinal))
+                    return false;
+                var parameters = method.GetParameters();
+                return parameters.Length == 4 &&
+                       parameters[0].ParameterType == typeof(OcctShape) &&
+                       parameters[1].ParameterType == typeof(OcctVector3d) &&
+                       parameters[2].ParameterType == typeof(double) &&
+                       parameters[3].ParameterType == typeof(System.Drawing.Color?);
+            });
+
+        Assert.IsNotNull(overload, "Viewer length dimensions must expose an explicit drafting-plane overload.");
+        Assert.AreEqual(typeof(OcctDimension), overload.ReturnType);
+    }
+}
